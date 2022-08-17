66e0c3b64afdf9a2825abb94e78a6618a0d23a8f
malubrandaor <malubrandaor@gmail.com>
2022-08-17 14:57:52 -0300

    requisito3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees.find(
+    (membroStaff) =>
+      membroStaff.firstName === employeeName ||
+      membroStaff.lastName === employeeName
+  );
 }
 
 module.exports = getEmployeeByName;
