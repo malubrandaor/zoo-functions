d2dc15fa4eaf2dd73637aa4164491a9d03575578
malubrandaor <malubrandaor@gmail.com>
2022-08-18 12:27:38 -0300

    requisito 12

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,42 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+// obtive ajuda para o desenvolvimento deste codigo 
+
+
+function Empregados(emp) {
+  const empID = emp.id;
+  const nComp = `${emp.firstName} ${emp.lastName}`;
+  const nAnimal = emp.responsibleFor.map((anilID) => species
+    .find((animal) => animal.id === aniID).name);
+  const localizacao = nAnimal.map((nome) => species
+    .find((animal) => animal.name === nome).location);
+
+  return {
+    id: empID,
+    fullName: nComp,
+    species: nAnimal,
+    locations: localizacao,
+  };
+}
+
+function getEmployeesCoverage(parametro) {
+  if (!p) {
+    return employees.map((emp) => Empregados(emp));
+  }
+  const { name, id } = p;
+  let resultado;
+  if (name) {
+    resultado = (objeto) => objeto.firstName === name || objeto.lastName === name;
+  } if (id) {
+    resultado = (objeto) => objeto.id === id;
+  }
+  const emp = employees.find(resultado);
+  if (!emp) {
+    throw new Error('Informações inválidas');
+  }
+  return Empregados(employee);
 }
 
 module.exports = getEmployeesCoverage;
