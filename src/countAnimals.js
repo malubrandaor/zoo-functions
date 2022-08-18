7cae088291048d12449bd562a9871fbfa1b55ca4
malubrandaor <malubrandaor@gmail.com>
2022-08-17 21:01:22 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  // if e reduce
+  if (!animal) {
+    return species.reduce((spe, quantidade) =>
+      Object.assign(spe, { [quantidade.name]: quantidade.residents.length }), {});
+  }
+  const lista = species.find(
+    (quantidade) => quantidade.name === animal.specie,
+  ).residents;
+
+  if (!animal.sex) {
+    return lista.length;
+  }
+  return lista.filter((todos) => todos.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
