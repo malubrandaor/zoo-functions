526c9c363df36c18ef584167eae4e6d5b6ef86c3
malubrandaor <malubrandaor@gmail.com>
2022-08-18 02:46:33 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const aniID = employees.find((funcionario) => funcionario.id === id).responsibleFor[0];
+
+  const ordem = species.find((animal) => animal.id === aniID).residents
+    .reduce((oldest, youngOne) => {
+      if (youngOne.age > oldest.age) {
+        return youngOne;
+      }
+      return oldest;
+    });
+  return [ordem.name, ordem.sex, ordem.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
